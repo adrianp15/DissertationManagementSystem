d1e6375d720d281f2ffc44c8c2bc0da09e84606b
AdrianP <adrianp1579@gmail.com>
2021-11-05 12:18:21 +0000

    Authentication add missing files

---
+++ src/main/java/com/university/dms/DissertationManagementSystem.java
@@ -0,0 +1,12 @@
+package com.university.dms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DissertationManagementSystem {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DissertationManagementSystem.class, args);
+    }
+}
