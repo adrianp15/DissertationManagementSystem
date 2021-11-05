c7aa0cbed758adfaaa4405e7d894e1a0fc0d4dde
AdrianP <adrianp1579@gmail.com>
2021-11-05 12:12:25 +0000

    Working authentication

--- src/main/java/com/university/dms/controller/AdminController.java
+++ src/main/java/com/university/dms/controller/AdminController.java
@@ -1,7 +1,7 @@
-package com.gpch.login.controller;
+package com.university.dms.controller;
 
-import com.gpch.login.model.User;
-import com.gpch.login.service.UserService;
+import com.university.dms.model.User;
+import com.university.dms.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
